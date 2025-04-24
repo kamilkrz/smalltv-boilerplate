10c933695ca2f50f37938890840e476fb3b58295
kamilkrz <kamil.krzes@circlekeurope.com>
2025-04-24 23:02:01 +0200

    Add splash screen

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@
 #include "apps/GooglyEyes/GooglyEyes.h"
 #include "apps/PomodoroApp/PomodoroApp.h"
 
-// Add MainMenu, GooglyEyes, and PomodoroApp to the collection
 void setup() {
   // SERIAL
   Serial.begin(115200);
@@ -23,15 +22,30 @@ void setup() {
   Display.init();
   Display.fillScreen(RGBto565(0, 60, 60));
   Display.setBacklight(255);
+
+  // Show splash screen
+  Display.setTextColor(RGBto565(255, 255, 255));
+  Display.setTextFont(2);
+  Display.setTextSize(2);
+  Display.setTextDatum(MC_DATUM);
+  Display.drawString("ESP8266 Clock", Display.width() / 2, Display.height() / 2);
+  // back to default
+  Display.setTextDatum(TL_DATUM);
+  Display.setTextSize(1);
+
+  delay(2000);  // Display splash screen for 2 seconds
+
   // Prep Piezo
   Piezo.beep(600, 200);
   delay(200);
 
   // Register apps
   AppCollection& appCollection = AppCollection::getInstance();
-  appCollection.addApp(&AppLauncher::getInstance());  // Main Menu
-  appCollection.addApp(&googlyEyes::getInstance());
+  // Main Menu Should be added first!
+  appCollection.addApp(&AppLauncher::getInstance());
+  // Then add other apps
   appCollection.addApp(&PomodoroApp::getInstance());
+  appCollection.addApp(&googlyEyes::getInstance());
 
   // Start with the first app
   appCollection.switchToApp(0);
