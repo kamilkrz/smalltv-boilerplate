ad3d1098ae4b85275a3ea44be94c78ce370026c3
kamilkrz <kamil.krzes@circlekeurope.com>
2025-05-14 13:26:12 +0200

    ✨ feat: improved WiFiManager integration

--- src/core/SettingsApp/SettingsApp.cpp
+++ src/core/SettingsApp/SettingsApp.cpp
@@ -8,13 +8,16 @@ SettingsApp& SettingsApp::getInstance() {
   return instance;
 }
 
-SettingsApp::SettingsApp() : App("Settings") {}
+SettingsApp::SettingsApp() : App("Settings") {
+}
 
-SettingsApp::~SettingsApp() {}
+SettingsApp::~SettingsApp() {
+}
 
 void SettingsApp::init() {
   exitApp = false;
-  Display.drawSplashScreen("Settings", "");
+  Display.drawSplashScreen("Settings", "connecting...");
+  wifiManager.setConfigPortalBlocking(true);
 
   // Dynamically create WiFiManager parameters based on registered settings
   if (parameters.empty()) {
@@ -32,68 +35,50 @@ void SettingsApp::init() {
       settings.dynamicSettings[param->getID()] = param->getValue();
     }
     settings.saveSettings();
-    Serial.println("Settings saved from callback.");
     wifiManager.stopConfigPortal();
     exitApp = true;
   });
   wifiManager.setWebServerCallback([]() {
-    // Display WiFi and settings information
     Display.fillScreen(TFT_BLACK);
     Display.setTextColor(TFT_WHITE);
     Display.setTextFont(4);
     Display.setCursor(0, 0);
     Display.println("Settings");
     Display.setTextFont(2);
-    Display.println("WiFi Name: " + WiFi.SSID());
-    Display.println("IP Address: " + WiFi.localIP().toString());
+      Display.println("WiFi Name: " + WiFi.SSID());
+      Display.println("IP Address: " + WiFi.localIP().toString());
     Display.println("Settings:");
     for (const auto& pair : settings.dynamicSettings) {
       Display.println(pair.first + ": " + pair.second);
     }
   });
 
-  // Attempt to connect to existing WiFi
+  wifiManager.setAPCallback([](WiFiManager* wm) {
+    Display.fillScreen(TFT_BLACK);
+    Display.setTextColor(TFT_WHITE);
+    Display.setTextFont(4);
+    Display.setCursor(0, 0);
+    Display.println("Settings");
+    Display.setTextFont(2);
+    Display.println("AP Name: " + String(wm->getConfigPortalSSID()));
+    Display.println("IP: " + WiFi.softAPIP().toString());
+    Display.println("Settings:");
+    for (const auto& pair : settings.dynamicSettings) {
+      Display.println(pair.first + ": " + pair.second);
+    }
+  });
+
+
   if (wifiManager.autoConnect("ESP8266_Settings")) {
-    Serial.println("WiFi connected.");
-    // Start the config portal in the local network
-    wifiManager.setConfigPortalBlocking(true);
-    wifiManager.startWebPortal();
-
-    // Keep the portal running until exitApp is set to true
-    while (wifiManager.getWebPortalActive()) {
-      wifiManager.process();  // Process WiFiManager tasks
-      delay(100);             // Avoid busy looping
-    }
-  } else {
-    Serial.println("Failed to connect to WiFi. Starting AP mode...");
-
-    // Configure AP mode
-    wifiManager.setAPCallback([](WiFiManager* wm) {
-      Display.fillScreen(TFT_BLACK);
-      Display.setTextColor(TFT_WHITE);
-      Display.setTextFont(4);
-      Display.setCursor(0, 0);
-      Display.println("Settings");
-      Display.setTextFont(2);
-      Display.println("AP Name: " + String(wm->getConfigPortalSSID()));
-      Display.println("IP: " + WiFi.softAPIP().toString());
-      Display.println("Settings:");
-      for (const auto& pair : settings.dynamicSettings) {
-        Display.println(pair.first + ": " + pair.second);
-      }
-    });
-
-    if (!wifiManager.startConfigPortal("ESP8266_Settings")) {
-      Serial.println("Failed to start AP mode. Connection is closed.");
-    }
+    wifiManager.startConfigPortal();
   }
+  
 }
 
 void SettingsApp::update() {
 }
 
 void SettingsApp::render() {
-  // No rendering needed
 }
 
 bool SettingsApp::shouldExit() {
