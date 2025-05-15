7826b40e78953d3a3ee15e9be78dc731b361b212
Kamil Krześ <104980405+kamilkrz@users.noreply.github.com>
2025-05-15 17:13:46 +0200

    ✨ feat: add CurrencyConverterApp for "real-time" currency exchange rates (#6)
    * ✨ feat: add CurrencyConverterApp for "real-time" currency exchange rates
    
    * ✨ feat: enhance CurrencyConverterApp with NTP support and improved caching logic

---
+++ src/apps/CurrencyConverterApp/CurrencyConverterApp.h
@@ -0,0 +1,30 @@
+#ifndef CURRENCY_CONVERTER_APP_H
+#define CURRENCY_CONVERTER_APP_H
+
+#include <Arduino.h>
+#include "core/App.h"
+
+class CurrencyConverterApp : public App {
+ public:
+  static CurrencyConverterApp& getInstance();
+  void init() override;
+  void update() override;
+  void render() override;
+  bool shouldExit() override;
+
+ private:
+  CurrencyConverterApp();
+  ~CurrencyConverterApp();
+  CurrencyConverterApp(const CurrencyConverterApp&) = delete;
+  CurrencyConverterApp& operator=(const CurrencyConverterApp&) = delete;
+
+  float exchangeRate = 0.0;
+  unsigned long lastApiCall = 0;
+  const unsigned long apiCallInterval = 3600000;  // 1 hour in milliseconds
+  void fetchExchangeRate();
+  bool shouldExitApp = false;
+  unsigned long lastRenderMillis = 0;
+  bool forceUpdate = false;
+};
+
+#endif  // CURRENCY_CONVERTER_APP_H
